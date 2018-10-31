bc4477e0cafeaae7505e6d6ecafa130514ecd677
Anjali203 <37259330+Anjali203@users.noreply.github.com>
2018-10-31 13:59:03 +0530

    Update adapters.rs

--- servers/src/common/adapters.rs
+++ servers/src/common/adapters.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! Adapters connecting new block, new transaction, and accepted transaction
+//! Adapters connecting new block, new transaction, and accepted transaction.
 //! events to consumers of those events.
 
 use std::fs::File;
